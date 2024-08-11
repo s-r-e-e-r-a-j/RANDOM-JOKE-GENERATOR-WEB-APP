4cccab629dd9da4936ff8bc125e8ff087db409c4
Sreeraj <154326486+C-O-D-E-G-A-M-E-R@users.noreply.github.com>
2024-08-11 13:17:36 +0530

    Update script.js

--- RANDOM JOKE GENERATOR/script.js
+++ RANDOM JOKE GENERATOR/script.js
@@ -2,17 +2,23 @@
 const output=document.querySelector("#output")
 const btn=document.querySelector("#btn")
 
+var speech=new SpeechSynthesisUtterance();
+speech.lang="en-US"||"en-GB"||"en-UK";
+speech.pitch=0.8;
+speech.rate=0.8;
+speech.volume=1;
 btn.addEventListener("click",async()=>{
 
-
 try{
+
 
 let api="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
 
 let response=await fetch(api)
 let data=await response.json();
-output.innerText=data.joke
-
+output.innerText=data.joke;
+speech.text=data.joke
+window.speechSynthesis.speak(speech)
 }catch(error){
 
 output.innerText="Error occured"
